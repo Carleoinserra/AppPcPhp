feac17be31d1b7433ff8acbb09b458f1d924763f
Carleoinserra <inserracarlo@gmail.com>
2024-06-20 09:52:49 +0200

    Add files via upload

---
+++ visualizza.php
@@ -0,0 +1,28 @@
+<?php
+// Collegati a un nuovo database (verrà creato se non esiste)
+$db = new SQLite3('MyDb.db');
+
+
+
+// Seleziona dati dalla tabella
+$query = 'SELECT * FROM computer';
+$result = $db->query($query);
+
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    echo "ID: " . $row['id'] . " - Modello: " . $row['modello'] . "<br>" . 
+    " - Marca: " . $row['marca'] . "<br>" . 
+    " - Prezzo: " . $row['prezzo'] . "<br>" . 
+    " - Quantità: " . $row['qnt'] . "<br>" . 
+    " - Vendite: " . $row['vendite'] . "<br>" . 
+    " - Immagine: <img src='" . $row['img'] . "' alt='Immagine del prodotto'><br>";
+
+}
+
+// Chiudi la connessione al database
+$db->close();
+?>
+<style>
+    img {
+        width: 300px;
+    }
+    </style>
